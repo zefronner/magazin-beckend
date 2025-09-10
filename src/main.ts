e632353297432f27289ae20f1c79d18915af8868
Muhammdrasul Shixnazarov <muhammadrasulshixnazarov@gmail.com>
2025-09-10 15:08:31 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('base');
   app.enableCors({
-    origin: 'http://localhost:3000',
-    'https://magazin-beckend-production.up.railway.app'
+    origin: ['http://localhost:3000',
+    'https://magazin-beckend-production.up.railway.app'],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000, () => {
